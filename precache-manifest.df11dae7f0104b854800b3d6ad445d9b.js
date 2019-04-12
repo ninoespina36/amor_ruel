bc76ccafecfe6586d52c4a0ccd907afdfbb50985
Your Name <you@example.com>
2019-04-12 18:52:23 +0800

    Updates

---
+++ precache-manifest.df11dae7f0104b854800b3d6ad445d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amor_ruel/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dacd32b4b827ca21cf8a",
+    "url": "/amor_ruel/static/js/runtime~main.116fbfc7.js"
+  },
+  {
+    "revision": "78718ca26387995edaa2",
+    "url": "/amor_ruel/static/js/main.d1842e4d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/amor_ruel/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "78718ca26387995edaa2",
+    "url": "/amor_ruel/static/css/main.f8430f94.chunk.css"
+  },
+  {
+    "revision": "3c1e099ec273821a8b90bd5df2dc805b",
+    "url": "/amor_ruel/index.html"
+  }
+];
