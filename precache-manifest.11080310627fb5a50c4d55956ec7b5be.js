d41975bcbfd8e4842e6c59f2f73d19d7b0032ead
Your Name <you@example.com>
2019-04-15 00:19:51 +0800

    Updates

---
+++ precache-manifest.11080310627fb5a50c4d55956ec7b5be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "309bacd632fc6cd27f2ee6fdee0f18bb",
+    "url": "/ruelandamor/static/media/ruel.309bacd6.png"
+  },
+  {
+    "revision": "c47caa6a5864f89f7cc86234912a1b14",
+    "url": "/ruelandamor/static/media/cover.c47caa6a.png"
+  },
+  {
+    "revision": "d7f073e0d199a2679f89318b1c85da63",
+    "url": "/ruelandamor/static/media/bg_.d7f073e0.png"
+  },
+  {
+    "revision": "34e088fb26994b920581562d3deb4fe9",
+    "url": "/ruelandamor/static/media/amor.34e088fb.png"
+  },
+  {
+    "revision": "5e94c8fbb1bcbdb6cfd0",
+    "url": "/ruelandamor/static/js/runtime~main.6840bab7.js"
+  },
+  {
+    "revision": "1a27210c2184792df624",
+    "url": "/ruelandamor/static/js/main.837449df.chunk.js"
+  },
+  {
+    "revision": "40cb802abfe13f64e045",
+    "url": "/ruelandamor/static/js/2.19eb89f5.chunk.js"
+  },
+  {
+    "revision": "1a27210c2184792df624",
+    "url": "/ruelandamor/static/css/main.cfc26dd2.chunk.css"
+  },
+  {
+    "revision": "43bdc2507e29b3a6f4c39a4d3d7e2aaa",
+    "url": "/ruelandamor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e481645571799f6cf76ad9cf59202b60.js"
+  "/ruelandamor/precache-manifest.11080310627fb5a50c4d55956ec7b5be.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ruelandamor/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
