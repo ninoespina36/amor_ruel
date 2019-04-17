162ba922aef3fa0e5f6017f69aa486504f12a7dd
Your Name <you@example.com>
2019-04-18 00:55:38 +0800

    gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,9 +1,233 @@
 import React from 'react';
 
+const openModal = () =>{
+
+}
+
+const currentSlide = (slide) =>{
+
+}
+
 const Gallery = () =>{
 	return(
 		<div className="container-fluid">
-			<p>To be continued...</p>
+
+			<div className="row justify-content-md-center mt-5">
+				<div className="col" align="center">
+					<h1 className="name">Photo Gallery</h1>
+				</div>
+			</div>
+
+			<div className="grid-padding mt-5">
+				<div className="row">
+					<div className="col-sm-4">
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0154.jpg")} className="fit-image" alt="slide-item" onClick={() =>{ openModal();currentSlide(1) }}	/>
+							</div>
+							
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0292.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(6) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0218.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(3) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0490.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0609.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0597.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0758.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0808.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0843.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(13) }}	/>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-sm-4">
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0279.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(5) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0345.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(7) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0208.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0511.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0454.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0737.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0746.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0646.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0565.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(2) }}	/>
+							</div>
+						</div>
+
+						<div className="row justify-content-sm-center gallery-title">
+							<div className="col mb-4" align="center">
+								<h1 className="amor_ruel_2 mt-5">Ruel &nbsp; <span className="and">&</span> &nbsp; Amor</h1>
+								<hr style={{backgroundColor:'#fff',width:'70%'}}/>
+								<span className="gallery-date">
+									<span>05<i className="fas fa-circle date-circle"></i>04<i className="fas fa-circle date-circle"></i>19</span>
+								</span>
+								<hr style={{backgroundColor:'#fff',width:'70%'}}/>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-sm-4">
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0396.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(9) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0265.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(4) }}	/>
+							</div>
+						</div>
+
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0359.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0443.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0513.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0479.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0764.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0882.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<img src={require("../images/gallery/DSC_0857.jpg")} className="fit-image" alt="slide-item" 
+									onClick={() =>{ openModal();currentSlide(8) }}	/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
