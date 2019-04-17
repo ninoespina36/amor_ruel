302ad40ca63c146ce829be35446a2822d0a78269
Your Name <you@example.com>
2019-04-18 01:42:45 +0800

    gallery changes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -14,118 +14,153 @@ const Gallery = () =>{
 				<div className="row">
 					<div className="col-sm-4">
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0154.jpg")} className="fit-image" alt="slide-item" />
-							</div>
-							
-						</div>
-
-						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0292.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0154.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0218.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0292.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0490.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0218.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0609.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0490.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0597.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0609.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0758.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0597.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0808.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0758.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0843.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0808.jpg")} className="fit-image" alt="slide-item" />
+								</div>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0843.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 					</div>
 
 					<div className="col-sm-4">
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0279.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0279.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0345.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0345.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0208.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0208.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0511.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0511.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0454.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0454.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0737.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0737.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0746.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0746.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0646.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0646.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0565.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0565.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row justify-content-sm-center gallery-title">
-							<div className="col mb-4 img-con" align="center">
+							<div className="col mb-4" align="center">
 								<h1 className="amor_ruel_2 mt-5">Ruel &nbsp; <span className="and">&</span> &nbsp; Amor</h1>
 								<hr style={{backgroundColor:'#fff',width:'70%'}}/>
 								<span className="gallery-date">
@@ -138,57 +173,75 @@ const Gallery = () =>{
 
 					<div className="col-sm-4">
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0396.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0396.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0265.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0265.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0359.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0359.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0443.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0443.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0513.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0513.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0479.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0479.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0764.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0764.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0882.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0882.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 
 						<div className="row">
-							<div className="col mb-4 img-con">
-								<img src={require("../images/gallery/DSC_0857.jpg")} className="fit-image" alt="slide-item" />
+							<div className="col mb-4">
+								<div className="img-con">
+									<img src={require("../images/gallery/DSC_0857.jpg")} className="fit-image" alt="slide-item" />
+								</div>
 							</div>
 						</div>
 					</div>
